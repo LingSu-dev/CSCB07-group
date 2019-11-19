3b1d3d10365e292ab36065d4e4a7736f35336ccb
yektamar <yektamar@UTORONTO.CA>
2019-11-19 00:18:39 +0000

    [Payam]:[Working][src_app][Added Android Src Folder]

---
+++ Project_phase2/phase2_feature/src_app/app/src/main/java/com/example/cscb07_app/EmployeeMakeAccount.java
@@ -0,0 +1,14 @@
+package com.example.cscb07_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeMakeAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_make_account);
+    }
+}
