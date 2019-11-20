eae4e858977ae14fd56d1cd2d2cb98c7fc528a51
zorbasai@UTORONTO.CA <zorbasai@UTORONTO.CA@511f5939-df6f-4e92-a61d-99f65ad0e2ac>
2019-11-20 03:30:07 +0000

    Created outline of cart saving/loading in salesApplication

--- Project_phase2/phase2_feature/src/com/b07/store/AccountHelper.java
+++ Project_phase2/phase2_feature/src/com/b07/store/AccountHelper.java
@@ -2,4 +2,19 @@ package com.b07.store;
 
 public class AccountHelper {
 
+  protected static boolean customerHasAccount(int userId) {
+    
+  }
+  
+  protected static boolean customerHasShoppingCarts(int userId) {
+    
+  }
+  
+  protected static ShoppingCart retrieveCustomerCart(int userId) {
+    
+  }
+  
+  protected static boolean saveCustomerCart(int userId, ShoppingCart cart) {
+    
+  }
 }
