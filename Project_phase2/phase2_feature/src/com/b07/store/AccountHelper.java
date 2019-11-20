df308c752c25620ff8ececb7553c4e63ebb02a59
zorbasai@UTORONTO.CA <zorbasai@UTORONTO.CA@511f5939-df6f-4e92-a61d-99f65ad0e2ac>
2019-11-20 03:40:21 +0000

    Implemented AccountHelper's customerHasAccount method

--- Project_phase2/phase2_feature/src/com/b07/store/AccountHelper.java
+++ Project_phase2/phase2_feature/src/com/b07/store/AccountHelper.java
@@ -1,9 +1,19 @@
 package com.b07.store;
 
+import com.b07.database.helper.DatabaseSelectHelper;
+import java.sql.SQLException;
+import java.util.List;
+
+
 public class AccountHelper {
 
-  protected static boolean customerHasAccount(int userId) {
+  protected static boolean customerHasAccount(int userId) throws SQLException {
+    List<Integer> accounts = DatabaseSelectHelper.getUserAccountsById(userId);
+    if (accounts == null) {
+      return false;
+    }
     
+    return !accounts.isEmpty();
   }
   
   protected static boolean customerHasShoppingCarts(int userId) {
