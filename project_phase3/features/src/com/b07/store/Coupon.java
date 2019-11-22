5adb47c0805731eae4b3929e0d4ed464488d4c8a
efimoval@UTORONTO.CA <efimoval@UTORONTO.CA@511f5939-df6f-4e92-a61d-99f65ad0e2ac>
2019-11-22 19:08:35 +0000

    begin coupon interface

---
+++ project_phase3/features/src/com/b07/store/Coupon.java
@@ -0,0 +1,32 @@
+package com.b07.store;
+
+import java.math.BigDecimal;
+import com.b07.inventory.Item;
+
+public interface Coupon {
+
+
+  /**
+   * Get the item the coupon is for
+   * @return the item, or null if there is no item for this coupon
+   */
+  public Item getItem();
+  
+  /**
+   * set this coupon's item to item
+   * @param item
+   */
+  public void setItem(Item item);
+  
+  /**
+   * Get the discount percentage as a BigDecimal
+   * @return the discount
+   */
+  public BigDecimal getDiscount();
+  
+  /**
+   * set the discount of this coupon
+   * @param discount: the discount
+   */
+  public void setDiscount(BigDecimal discount);
+}
