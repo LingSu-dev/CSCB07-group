b016261590d317713fc437f3829f2d0dd8c37bda
yektamar <yektamar@UTORONTO.CA>
2019-11-23 17:23:47 +0000

    Added Intilization Controller

---
+++ project_phase3/features/src_app/app/src/main/java/com/example/cscb07_app/Controller/InitializationController.java
@@ -0,0 +1,19 @@
+package com.example.cscb07_app.Controller;
+
+import android.content.Context;
+import android.view.View;
+
+public class InitializationController implements View.OnClickListener {
+
+  private Context appContext;
+
+  public InitializationController(Context context) {
+    this.appContext = context;
+  }
+
+  @Override
+  public void onClick(View view) {
+
+  }
+
+}
