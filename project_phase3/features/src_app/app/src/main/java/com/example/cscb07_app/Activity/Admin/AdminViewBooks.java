a2fed7aa6e4fbafd7831f06255fb9b3d24dca3e3
yektamar@UTORONTO.CA <yektamar@UTORONTO.CA@511f5939-df6f-4e92-a61d-99f65ad0e2ac>
2019-11-25 02:56:08 +0000

    Added Activity For View Books

---
+++ project_phase3/features/src_app/app/src/main/java/com/example/cscb07_app/Activity/Admin/AdminViewBooks.java
@@ -0,0 +1,14 @@
+package com.example.cscb07_app.Activity.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.cscb07_app.R;
+
+public class AdminViewBooks extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_admin_view_books);
+  }
+}
