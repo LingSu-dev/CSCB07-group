c16514bacf19d08d6b3e5031d559b3827d128bd1
zorbasai@UTORONTO.CA <zorbasai@UTORONTO.CA@511f5939-df6f-4e92-a61d-99f65ad0e2ac>
2019-11-28 03:19:11 +0000

    Removed unused method in Account class

--- project_phase3/features/src/com/b07/users/Account.java
+++ project_phase3/features/src/com/b07/users/Account.java
@@ -42,43 +42,6 @@ public class Account {
   } 
   
   /**
-   * Retrieve a cart of the item's stored in ALL of the user's accounts.
-   *
-   * @param userId the user who's items will be retrieved.
-   * @return True if successful, false if no such cart can be made.
-   * @throws SQLException if there is an issue communicating with the database.
-   */
-  public boolean retrieveCustomerCartCombined() throws SQLException {
-    List<ShoppingCart> carts = new ArrayList<ShoppingCart>();
-    List<Integer> ids = DatabaseSelectHelper.getUserAccountsById(userId);
-    User user = DatabaseSelectHelper.getUserDetails(userId);
-    if (ids == null || user == null || !(user instanceof Customer)) {
-      return false;
-    }
-
-    // Populate list with the items stored in all of a user's accounts.
-    for (int i = 0; i < ids.size(); i++) {
-      carts.add(DatabaseSelectHelper.getAccountDetails(ids.get(i)));
-    }
-    // Combine all the items into one shopping cart to be returned.
-    ShoppingCart combinedCart = new ShoppingCart((Customer) user);
-    // For each shopping cart
-    for (int j = 0; j < carts.size(); j++) {
-      // Get items contained within
-      if (carts.get(j) != null) {
-        HashMap<Item, Integer> items = carts.get(j).getItemsWithQuantity();
-        // Add each item to the combined cart.
-        for (Item item : items.keySet()) {
-          combinedCart.addItem(item, items.get(item));
-        }
-      }
-    }
-
-    cart = combinedCart;
-    return true;
-  }
-  
-  /**
    * Saves a customer's cart to their lowest numbered account.
    *
    * @param userId the user to save the cart of.
