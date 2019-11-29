96fc2885c6ef5e957a5193e45ac9a0e5b03a1b60
yektamar@UTORONTO.CA <yektamar@UTORONTO.CA@511f5939-df6f-4e92-a61d-99f65ad0e2ac>
2019-11-29 00:44:50 +0000

    Added File

---
+++ project_phase3/features/src_app/app/src/main/java/com/example/cscb07_app/Src/database/helper/DatabaseMethodHelper.java
@@ -0,0 +1,15 @@
+package com.example.cscb07_app.Src.database.helper;
+
+import android.content.Context;
+import com.example.cscb07_app.Src.database.DatabaseDriverAndroid;
+
+public class DatabaseMethodHelper extends DatabaseDriverAndroid {
+
+  public DatabaseMethodHelper(Context context) {
+    super(context);
+  }
+  
+  public long insertRole(String role) {
+    return super.insertRole(role);
+  }
+}
