2743b212bbc7569246a4ae4c132a727181c7982e
yektamar <yektamar@UTORONTO.CA>
2019-11-30 16:45:14 +0000

    Created DialogController

---
+++ project_phase3/features/src_app/app/src/main/java/com/example/cscb07_app/Controller/DialogController.java
@@ -0,0 +1,34 @@
+package com.example.cscb07_app.Controller;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import com.example.cscb07_app.Activity.Initialization.InitializationCreateFirstEmployee;
+import com.example.cscb07_app.Activity.Login.LoginMenu;
+
+public class DialogController implements DialogInterface.OnClickListener {
+
+  private Context appContext;
+  private DialogId id;
+
+  public DialogController(Context context, DialogId id) {
+    this.appContext = context;
+    this.id = id;
+  }
+
+  @Override
+  public void onClick(DialogInterface dialog, int which) {
+    switch(id){
+      case CREATE_FIRST_ADMIN_DETAILS:
+        appContext.startActivity(new Intent(appContext, InitializationCreateFirstEmployee.class));
+        break;
+      case CREATE_FIRST_EMPLOYEE_DETAILS:
+        appContext.startActivity(new Intent(appContext, LoginMenu.class));
+        break;
+      case LOGIN_INCORRECT_CREDENTIALS:
+        break;
+      case AGE_EMPTY_DIALOG:
+        break;
+    }
+  }
+}
---
+++ project_phase3/features/src_app/app/src/main/java/com/example/cscb07_app/Controller/DialogId.java
@@ -0,0 +1,5 @@
+package com.example.cscb07_app.Controller;
+
+public enum DialogId {
+  CREATE_FIRST_ADMIN_DETAILS, CREATE_FIRST_EMPLOYEE_DETAILS, LOGIN_INCORRECT_CREDENTIALS, AGE_EMPTY_DIALOG
+}
