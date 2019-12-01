e4d83e383c8d8d8437bec10d91d2b5ecaf16ecb0
zorbasai@UTORONTO.CA <zorbasai@UTORONTO.CA@511f5939-df6f-4e92-a61d-99f65ad0e2ac>
2019-12-01 00:32:22 +0000

    Added reInitialize helper to desktop app

--- project_phase3/features/src/com/b07/database/helper/DatabaseDriverHelper.java
+++ project_phase3/features/src/com/b07/database/helper/DatabaseDriverHelper.java
@@ -1,6 +1,8 @@
 package com.b07.database.helper;
 
 import com.b07.database.DatabaseDriver;
+import com.b07.exceptions.ConnectionFailedException;
+
 import java.sql.Connection;
 
 /**
@@ -15,5 +17,9 @@ public class DatabaseDriverHelper extends DatabaseDriver {
 
   protected static Connection connectOrCreateDataBase() {
     return DatabaseDriver.connectOrCreateDataBase();
+  }
+  
+  protected static Connection reInitialize() throws ConnectionFailedException {
+    return DatabaseDriver.reInitialize();
   }
 }
