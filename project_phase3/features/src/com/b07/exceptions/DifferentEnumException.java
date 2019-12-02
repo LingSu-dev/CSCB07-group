98ee4971354e011060b0f73108c8253baa006084
sulingfe <sulingfe@UTORONTO.CA>
2019-12-02 02:36:58 +0000

    Added javadoc to EnumException

--- project_phase3/features/src/com/b07/exceptions/DifferentEnumException.java
+++ project_phase3/features/src/com/b07/exceptions/DifferentEnumException.java
@@ -1,9 +1,14 @@
 package com.b07.exceptions;
 
+/**
+ * exception used for when the Enums of two instances of the software do not match
+ * @author lingf
+ *
+ */
 public class DifferentEnumException extends Exception {
 
   /**
-   * When Enums does not match
+   * Id of exception
    */
   private static final long serialVersionUID = 9125542791582765711L;
 
