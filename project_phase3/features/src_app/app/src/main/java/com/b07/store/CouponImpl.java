d3ad0e27bfe1054fc91c1ed6a1f68c0bce1b5d69
zorbasai <zorbasai@UTORONTO.CA>
2019-12-02 02:08:54 +0000

    Added Serialize Package to Android App

--- project_phase3/features/src_app/app/src/main/java/com/b07/store/CouponImpl.java
+++ project_phase3/features/src_app/app/src/main/java/com/b07/store/CouponImpl.java
@@ -1,20 +1,15 @@
 package com.b07.store;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class CouponImpl implements Coupon, Serializable{
-  
-  /**
-   * Serial Version ID of CouponImpl.
-   */
-  private static final long serialVersionUID = 1L;
+public class CouponImpl implements Coupon {
+
   int item;
   int uses;
   String type;
   BigDecimal discount;
   String code;
-  
+
   public CouponImpl(int item, int uses, String type, BigDecimal discount, String code) {
     this.item = item;
     this.uses = uses;
@@ -25,61 +20,51 @@ public class CouponImpl implements Coupon, Serializable{
 
   @Override
   public int getItemId() {
-    // TODO Auto-generated method stub
     return item;
   }
 
   @Override
   public void setItemId(int item) {
-    // TODO Auto-generated method stub
     this.item = item;
   }
 
   @Override
   public BigDecimal getDiscount() {
-    // TODO Auto-generated method stub
     return this.discount;
   }
 
   @Override
   public void setDiscount(BigDecimal discount) {
-    // TODO Auto-generated method stub
     this.discount = discount;
   }
 
   @Override
   public String getCode() {
-    // TODO Auto-generated method stub
     return this.code;
   }
 
   @Override
   public void setCode(String code) {
-    // TODO Auto-generated method stub
     this.code = code;
   }
 
   @Override
   public String getType() {
-    // TODO Auto-generated method stub
     return this.type;
   }
 
   @Override
   public void setType(String type) {
-    // TODO Auto-generated method stub
     this.type = type;
   }
 
   @Override
   public int getUses() {
-    // TODO Auto-generated method stub
     return this.uses;
   }
 
   @Override
   public void setUses(int uses) {
-    // TODO Auto-generated method stub
     this.uses = uses;
   }
 
